4107d9e885634d7509895c4dc583960e6f8baf5a
ignaciovi <ignvaldelvira@gmail.com>
2019-10-22 20:53:28 +0100

    React flask setup

--- pals-songmix/README.md
+++ pals-songmix/README.md
@@ -5,15 +5,16 @@ python run.py
 DONE
 S-1 Given an artist, get top tracks
 S-2 Create playlist from website
+S-3 Implement React
 
 BACKLONG
-S-3 Implement React
-S-4 Be able to create array of songs from multiple artists given
-S-5 Be able to take not only top tracks but random tracks
-S-6 Limit size of playlist depending on number of artists given
-S-7 Style website
-S-8 Be able to search for less popular similar artists
-S-9 Autocomplete search and verification for input
+S-4 Simple call react flask
+S-5 Be able to create array of songs from multiple artists given
+S-6 Be able to take not only top tracks but random tracks
+S-7 Limit size of playlist depending on number of artists given
+S-8 Style website
+S-9 Be able to search for less popular similar artists
+S-10 Autocomplete search and verification for input
 
 
 # React
--- pals-songmix/songbinatio/src/App.tsx
+++ pals-songmix/songbinatio/src/App.tsx
@@ -2,69 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
-
-// interface IStateProps {
-//   count: number;
-//   name:string;
-// };
-
-// interface IDispatchProps {
-//  setName: (name:string) => any
-// };
-
-// export default class App extends React.Component<any,IStateProps> {
-//   constructor(props:IDispatchProps) {
-//     super(props);
-//     this.state = {
-//       count:0,
-//       name:"",
-//     }
-//     this.setName = this.setName.bind(this)
-//   }
-
-//   increment = () => {
-//     this.setState({
-//       count: (this.state.count + 1)
-//     });
-//   };
-
-//   decrement = () => {
-//     this.setState({
-//       count: (this.state.count - 1)
-//     });
-//   };
-
-//   setName(name:string) {
-//     this.setState({
-//       name
-//     });
-//   }
-
-//   render () {
-//     return (
-//       <div>
-//         <h1>{this.props.count}</h1>
-//         <button onClick={this.increment}>Increment</button>
-//         <button onClick={this.decrement}>Decrement</button>
-//         <div>
-//          <input type="text" name="name" onChange={(event) => this.props.setName(event.target.value)}/>
-//          <p>{this.props.name}</p>
-//        </div>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
-
-// export default connect<IStateProps, IDispatchProps, {}>(App);
-
-// export const FCCounterConnected = connect(
-//   IStateProps,
-//   IDispatchProps
-// )(App);
-
 interface IDispatchProps { 
   
 };
